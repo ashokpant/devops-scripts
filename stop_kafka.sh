9a37b45162e778c4046ae36f71de8dbe971b7f59
Ashok Kumar Pant <asokpant@gmail.com>
2023-12-20 20:48:49 +0545

    Add kafka

--- stop_kafka.sh
+++
@@ -1,1 +0,0 @@
-docker compose -f /home/ashok/Projects/scripts/kafka-docker-compose.yml down --remove-orphans 
---
+++ zookeeper.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+SERVICE_NAME=zookeeper
+DOCKER_COMPOSE_FILE=./zookeeper-docker-compose.yml
+
+start() {
+    echo "Starting $SERVICE_NAME service ..."
+    docker compose -f $DOCKER_COMPOSE_FILE up -d 
+}
+
+stop() {
+    echo "Stopping $SERVICE_NAME service ..."
+    docker compose -f $DOCKER_COMPOSE_FILE down  --remove-orphans
+}
+
+# Check the command-line argument to determine action
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        stop
+        start
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart}"
+        exit 1
+        ;;
+esac
+
+exit 0
